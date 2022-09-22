1d46dda1a020a042011ce6e6e78112ace97ffc02
Akashmaran <110605201+Akashmaran@users.noreply.github.com>
2022-09-22 20:44:14 +0530

    Delete assignment 2/Team Member 1 directory

--- assignment 2/Team Member 1/temp.py
+++
@@ -1,9 +0,0 @@
-import random
-#temperature generation
-t=random.randint(1,300)
-#humidity generation
-h=random.randint(1,50)
-if(t>150):
-    print("ALERT!, Temperature is High")
-else:
-    print("NORMAL, Temperature is Fine")
